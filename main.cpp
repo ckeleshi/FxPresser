e949ccc211825b92f91ae9f52414dad944538649
ckeleshi <clanschen@gmail.com>
2026-08-14 17:38:03 +0800

    修改项目为静态编译 改回Qt5以兼容Win7 增加正确的请求管理员权限方法

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
     TOKEN_PRIVILEGES tp;
     OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken);
     tp.PrivilegeCount = 1;
-    LookupPrivilegeValueW(nullptr, SE_DEBUG_NAME, &tp.Privileges[0].Luid);
+    LookupPrivilegeValue(nullptr, SE_DEBUG_NAME, &tp.Privileges[0].Luid);
     tp.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
     AdjustTokenPrivileges(hToken, FALSE, &tp, sizeof(tp), nullptr, nullptr);
     CloseHandle(hToken);
