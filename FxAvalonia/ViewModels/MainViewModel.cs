ed39c150adfd0173eb003bd1592e83f6dd6effc7
ckeleshi <clanschen@gmail.com>
2026-09-10 19:54:48 +0800

    删除测试项目

--- FxAvalonia/ViewModels/MainViewModel.cs
+++
@@ -1,9 +0,0 @@
-using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace FxAvalonia.ViewModels;
-
-public partial class MainViewModel : ViewModelBase
-{
-    [ObservableProperty]
-    public partial string Greeting { get; set; } = "Welcome to Avalonia!";
-}
--- FxAvalonia/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace FxAvalonia.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}
